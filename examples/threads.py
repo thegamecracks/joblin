ad96810de762c86b8072dfd8139c809b346c21cf
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2024-05-04 10:43:34 -0400

    docs: add threads.py interrupt support

--- examples/threads.py
+++ examples/threads.py
@@ -4,6 +4,8 @@ Each thread will concurrently read jobs from the queue.
 
 """
 
+from __future__ import annotations
+
 import random
 import threading
 import time
@@ -19,16 +21,10 @@ def main() -> None:
     with scheduler_factory() as scheduler:
         submit_jobs(scheduler)
 
-    threads = [
-        threading.Thread(target=run_pending_jobs, args=(scheduler_factory,))
-        for _ in range(3)
-    ]
+    runners = [Runner(scheduler_factory) for _ in range(3)]
+    threads = [threading.Thread(target=runner.run_pending_jobs) for runner in runners]
 
-    for t in threads:
-        t.start()
-
-    for t in threads:
-        t.join()
+    start_and_join(runners, threads)
 
     with scheduler_factory() as scheduler:
         pending = scheduler.count_pending_jobs()
@@ -46,30 +42,58 @@ def submit_jobs(scheduler: Scheduler) -> None:
     print(f"{n_jobs} jobs submitted, {pending} jobs pending")
 
 
-def run_pending_jobs(scheduler_factory: Callable[[], Scheduler]) -> None:
-    runner = Runner(scheduler_factory())
-    runner.run_pending_jobs()
+def start_and_join(runners: list[Runner], threads: list[threading.Thread]) -> None:
+    try:
+        for t in threads:
+            t.start()
+
+        join(threads)
+    except BaseException as e:
+        # This is how we propagate signals like KeyboardInterrupt
+        # from the main thread to the runners.
+        print(f"{type(e).__name__} received, stopping runners")
+        for runner in runners:
+            runner.stop()
+        raise
+    finally:
+        join(threads)
+
+
+def join(threads: list[threading.Thread]) -> None:
+    # For whatever reason, a bare .join() call seems to prevent
+    # signals from coming through.
+    while any(t.is_alive() for t in threads):
+        time.sleep(0.25)
 
 
 class Runner:
-    def __init__(self, scheduler: Scheduler) -> None:
-        self.scheduler = scheduler
+    def __init__(self, scheduler_factory: Callable[[], Scheduler]) -> None:
+        self.scheduler_factory = scheduler_factory
+        self._stop_ev = threading.Event()
 
     def run_pending_jobs(self) -> None:
-        # For this example, we'll stop checking for jobs once the scheduler is empty.
-        # Feel free to make this run indefinitely with a stop event.
-        while True:
-            job_delay = self.scheduler.lock_next_job_delay()
+        scheduler = self.scheduler_factory()
+
+        # For this example, we'll stop checking for jobs once
+        # the stop event is set or the scheduler is empty.
+        # Feel free to make this run indefinitely.
+        while not self._stop_ev.is_set():
+            job_delay = scheduler.lock_next_job_delay()
             if job_delay is None:
                 break
 
             job_id, delay = job_delay
 
-            with self._failsafe_unlock(job_id):
-                self._wait_to_run_job(job_id, delay)
+            with self._failsafe_unlock(scheduler, job_id):
+                self._wait_to_run_job(scheduler, job_id, delay)
+
+        print("Done")
+
+    def stop(self) -> None:
+        self._stop_ev.set()
 
     @contextmanager
-    def _failsafe_unlock(self, job_id: int) -> Iterator[None]:
+    def _failsafe_unlock(self, scheduler: Scheduler, job_id: int) -> Iterator[None]:
         # In case an error occurs, unlock the job.
         #
         # Yes, this is a bit confusing to think about and is still
@@ -80,25 +104,29 @@ class Runner:
             yield
         except BaseException:
             print(f"Failsafe! Unlocking job #{job_id}")
-            self.scheduler.unlock_job(job_id)
+            scheduler.unlock_job(job_id)
             raise
 
-    def _wait_to_run_job(self, job_id: int, delay: float) -> None:
+    def _wait_to_run_job(self, scheduler: Scheduler, job_id: int, delay: float) -> None:
         if delay > 0:
             print(f"Waiting {delay:.2f}s for job #{job_id}...")
-            time.sleep(delay)
 
-        job = self.scheduler.get_job_by_id(job_id)
+            if self._stop_ev.wait(delay):
+                return print("Stop requested while waiting on job")
+
+        job = scheduler.get_job_by_id(job_id)
         if job is None:
             return print(f"Skipping now-deleted job #{job_id}")
 
-        self._run_job(job)
+        # At this point, the runner can't check the stop signal
+        # and the job must be run to completion.
+        self._run_job(scheduler, job)
 
-    def _run_job(self, job: Job) -> None:
+    def _run_job(self, scheduler: Scheduler, job: Job) -> None:
         print(f"Running job #{job.id}...")
 
         time.sleep(random.uniform(1, 4))
-        self.scheduler.complete_job(job.id)
+        scheduler.complete_job(job.id)
 
         print(f"Completed job #{job.id}")
 
